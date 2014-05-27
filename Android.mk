814dba948b48a386ea201d902e3a0e622b1f7896
tux-mind <massimo.dragano@gmail.com>
2014-05-27 03:01:13 +0200

    libgnu has been removed

--- Android.mk
+++ Android.mk
@@ -1,30 +1,5 @@
 LOCAL_PATH := $(call my-dir)
 
-#original path: lib/gl/libgnu.la
-include $(CLEAR_VARS)
-
-LOCAL_CFLAGS:= -DHAVE_CONFIG_H -fvisibility=hidden -ffunction-sections -fdata-sections
-
-LOCAL_C_INCLUDES:= \
-	$(LOCAL_PATH)/lib/gl\
-	$(LOCAL_PATH)\
-	external/libiconv/include
-LOCAL_SRC_FILES:= \
-	lib/gl/c-ctype.c\
-	lib/gl/c-strcasecmp.c\
-	lib/gl/c-strncasecmp.c\
-	lib/gl/striconv.c\
-	lib/gl/unistr/u8-mbtoucr.c\
-	lib/gl/unistr/u8-uctomb.c\
-	lib/gl/unistr/u8-uctomb-aux.c\
-	lib/gl/strverscmp.c
-LOCAL_STATIC_LIBRARIES:= \
-	libiconv
-LOCAL_MODULE := libgnu
-
-include $(BUILD_STATIC_LIBRARY)
-
-
 #original path: lib/libidn.la
 include $(CLEAR_VARS)
 
@@ -38,6 +13,14 @@ LOCAL_C_INCLUDES:= \
 	$(LOCAL_PATH)/lib/gl\
 	external/libiconv/include
 LOCAL_SRC_FILES:= \
+	lib/gl/c-ctype.c\
+	lib/gl/c-strcasecmp.c\
+	lib/gl/c-strncasecmp.c\
+	lib/gl/striconv.c\
+	lib/gl/unistr/u8-mbtoucr.c\
+	lib/gl/unistr/u8-uctomb.c\
+	lib/gl/unistr/u8-uctomb-aux.c\
+	lib/gl/strverscmp.c\
 	lib/nfkc.c\
 	lib/toutf8.c\
 	lib/version.c\
@@ -56,31 +39,8 @@ LOCAL_SRC_FILES:= \
 	lib/tld.c\
 	lib/tlds.c
 LOCAL_STATIC_LIBRARIES:= \
-	libiconv\
-	libgnu
+	libiconv
 LOCAL_MODULE := libidn
-
-include $(BUILD_STATIC_LIBRARY)
-
-
-#original path: src/libidn_cmd.la
-include $(CLEAR_VARS)
-
-LOCAL_CFLAGS:= -DHAVE_CONFIG_H \
--DLOCALEDIR=\"/opt/android-ndk/platforms/android-18/arch-arm/usr/share/locale\" \
--ffunction-sections -fdata-sections
-
-LOCAL_C_INCLUDES:= \
-	$(LOCAL_PATH)/src\
-	$(LOCAL_PATH)\
-	$(LOCAL_PATH)/lib\
-	$(LOCAL_PATH)/gl\
-	external/libiconv/include
-LOCAL_SRC_FILES:= \
-	src/idn_cmd.c
-LOCAL_STATIC_LIBRARIES:= \
-	libgnu
-LOCAL_MODULE := libidn_cmd
 
 include $(BUILD_STATIC_LIBRARY)
 
